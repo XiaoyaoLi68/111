f69ad4ec9059b3b3a59e0ab609d5185f7607d7fe
kingbo <1873270473@qq.com>
2026-01-10 11:56:09 +0800

    初始化项目

---
+++ src/test/java/com/kingbo/petserver/PetServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingbo.petserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
